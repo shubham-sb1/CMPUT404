0f80109ccaea30ce400a0bf17326312204e17bec
Shubham <86523618+shubham-sb1@users.noreply.github.com>
2022-09-13 14:08:10 -0600

    Update lab.py

--- labs/lab01/lab.py
+++ labs/lab01/lab.py
@@ -1,5 +1,5 @@
 import requests
 print(f"Request version : {requests.__version__} ")
-print( requests.get('http://www.google.com'))
+requests.get('http://www.google.com')
 print('Source code below:')
 print(requests.get("https://raw.githubusercontent.com/shubham-sb1/CMPUT404/main/labs/lab01/lab.py").text)
